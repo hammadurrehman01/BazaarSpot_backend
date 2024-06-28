5048f4cf73a43bdbfd49fe07d7df5ea2a24489b6
hammaduurehman01 <hammmadurrehman@techlabs.cc>
2024-06-28 20:27:17 +0500

    fix: user model access and refresh token functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -31,3 +31,39 @@ const userSchema = new Schema({
 },
     { timestamps: true }
 )
+
+userSchema.pre("save", async (next) => {
+    if (!this.isModified("password")) return next()
+
+    this.password = await bcyrpt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async (password) => {
+    return await bcyrpt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = () => {
+    return jwt.sign({
+        _id: this._id,
+        name: this.name,
+        email: this.email,
+        phone: this.phone,
+    }, process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateAccessToken = () => {
+    return jwt.sign({
+        _id: this._id,
+    }, process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model('User', userSchema)

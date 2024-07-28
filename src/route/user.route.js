a0f7f1ba11661ec7cba08515cc9924837953791c
hammadurrehman01 <hammadurrehman@techlabs.cc>
2024-07-28 23:15:00 +0500

    removed middleware from forgot api

--- src/route/user.route.js
+++ src/route/user.route.js
@@ -9,7 +9,7 @@ userRouter.route("/login").post(loginUser);
 userRouter.route("/logout").post(verifyJWT, logoutUser);
 userRouter.route("/current-user").get(verifyJWT, getCurrentUser);
 userRouter.route("/update-user").put(verifyJWT, updateUserDetails);
-userRouter.route("/forgot-password").post(verifyJWT, forgotPassword);
+userRouter.route("/forgot-password").post(forgotPassword);
 userRouter.route("/reset-password").post(resetPassword);
 
 export default userRouter;
